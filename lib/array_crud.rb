bacae4a5ba7a7b69d1df8088adef2e0a8a54e9aa
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-03-28 13:38:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,7 +35,7 @@ end
 
 def retrieve_first_element_from_array(array)
   dark_souls_3 = ["wow", "younger prince lothric", "am", "arrays!"]
-  create_an_array.index ("am")
+  create_an_array
   
 end
 
