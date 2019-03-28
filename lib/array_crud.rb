dfd58d3f3c34762a3cfacb8fe6ef2f60612d6f65
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-03-28 13:39:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,8 +34,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  dark_souls_3 = ["wow", "younger prince lothric", "am", "arrays!"]
-  create_an_array[2]
+  dark_souls_3 = ["am", "younger prince lothric", "wow", "arrays!"]
+  create_an_array[0]
   
 end
 
