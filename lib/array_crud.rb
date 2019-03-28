6c5e9faaaccb25363e83ba3fcc2b5652bb67e4a0
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-03-28 13:01:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,18 +24,21 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  elder prince lorian = dark_souls_3.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
+  dark_souls_3 [2]
   
 end
 
 def retrieve_first_element_from_array(array)
+  dark_souls_3 [1]
   
 end
 
 def retrieve_last_element_from_array(array)
+  dark_souls_3 [4]
   
 end
